cad4b82424bd25c08187bec5e592147a46763bce
charlieliao3rd <cliaotutoring@gmail.com>
2025-07-16 13:26:18 +0100

    Update vercel.js

--- vercel.js
+++ vercel.js
@@ -1,34 +1,48 @@
 {
-    "headers": [
+  "headers": [
+    {
+      "source": "/sw.js",
+      "headers": [
         {
-            "source": "/sw.js",
-            "headers": [
-                {
-                    "key": "Service-Worker-Allowed",
-                    "value": "/"
-                },
-                {
-                    "key": "Content-Type",
-                    "value": "application/javascript"
-                },
-                {
-                    "key": "Cache-Control",
-                    "value": "no-cache, max-age=0"
-                },
-                {
-                    "key": "Access-Control-Allow-Origin",
-                    "value": "*"
-                }
-            ]
+          "key": "Service-Worker-Allowed",
+          "value": "/"
         },
         {
-            "source": "/(.*)",
-            "headers": [
-                {
-                    "key": "Content-Security-Policy",
-                    "value": "default-src 'self' https: 'unsafe-inline' 'unsafe-eval'; connect-src 'self' https://*.braze.com; worker-src 'self' blob:;"
-                }
-            ]
+          "key": "Content-Type",
+          "value": "application/javascript"
+        },
+        {
+          "key": "Cache-Control",
+          "value": "no-cache, max-age=0"
+        },
+        {
+          "key": "Access-Control-Allow-Origin",
+          "value": "*"
         }
-    ]
+      ]
+    },
+    {
+      "source": "/(.*)",
+      "headers": [
+        {
+          "key": "Content-Security-Policy",
+          "value": "default-src 'self' https: 'unsafe-inline' 'unsafe-eval'; connect-src 'self' https://*.braze.com; worker-src 'self' blob:;"
+        }
+      ]
+    }
+  ],
+  "routes": [
+    {
+      "src": "/push-test",
+      "methods": ["GET"],
+      "headers": {
+        "x-vercel-protection-bypass": "1"
+      },
+      "dest": "/index.html"
+    },
+    {
+      "src": "/(.*)",
+      "dest": "/index.html"
+    }
+  ]
 }
